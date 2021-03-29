fb81e77e8144820da915679c76690ac9b6f36d45
John <hello@johnsalcedo.co>
2021-03-29 23:51:54 +0100

    Initialise Typescript

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <span>it do be like that</span>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
